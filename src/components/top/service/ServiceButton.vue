bb2a1b5941e4d7c6be0feb600a10af5a46a77b9b
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2021-05-31 13:29:28 +0900

    ・サービスの子コンポーネント（サービスボタン）を実装する。テストコードのみの実装でコンポーネントの実装は別途実施予定。

--- src/components/top/service/ServiceButton.vue
+++ src/components/top/service/ServiceButton.vue
@@ -1,12 +1,43 @@
 <template>
-  <div></div>
+  <div
+    @mouseover="() => isHover = true"
+    @mouseout="() => isHover = false">
+    <a
+      class="ServiceButtonLink"
+      :href="data.url">
+      <div>
+        <div v-if="data.icon">
+          <FontAwesomeIcon
+            :icon="data.icon"
+            :style="{
+              color: isHover ? '#fff' : data.titleColor
+            }" />
+        </div>
+        <div
+          :style="{
+            color: isHover ? '#fff' : data.titleColor
+          }">{{ data.title }}
+        </div>
+      </div>
+    </a>
+  </div>
 </template>
 
 <script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 export default {
   name: 'ServiceButton',
   props: {
     data: Object
+  },
+  components: {
+    FontAwesomeIcon
+  },
+  data () {
+    return {
+      isHover: false
+    }
   }
 }
 </script>
---
+++ src/components/top/service/__tests__/ServiceButton.spec.js
@@ -0,0 +1,38 @@
+import { service } from '@/components/top/service/ServiceData'
+import { shallowMount } from '@vue/test-utils'
+import ServiceButton from '@/components/top/service/ServiceButton'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fromStringStyleToObject } from '@/test/utils'
+
+describe('ServiceButtonコンポーネント', () => {
+  it('プロップスのテスト', () => {
+    const data = service.buttonGroup1[0]
+    const wrapper = shallowMount(
+      ServiceButton,
+      {
+        propsData: {
+          data
+        }
+      }
+    )
+
+    const serviceButtonLinkNode = wrapper.findAll('a.ServiceButtonLink')
+    expect(serviceButtonLinkNode).toHaveLength(1)
+    expect(serviceButtonLinkNode.at(0).attributes().href).toEqual(data.url)
+
+    const serviceButtonIconNode = wrapper.findAllComponents(FontAwesomeIcon)
+    if (data.icon) {
+      expect(serviceButtonIconNode).toHaveLength(1)
+      expect(serviceButtonIconNode.at(0).attributes().icon.split(',')).toEqual(data.icon)
+    } else {
+      expect(serviceButtonIconNode).toHaveLength(0)
+    }
+
+    const serviceButtonTitleNode = wrapper.findAll('div.ServiceButtonTitle')
+    expect(serviceButtonTitleNode).toHaveLength(1)
+    expect(serviceButtonTitleNode.at(0).text()).toEqual(data.title)
+    expect(serviceButtonTitleNode.at(0).attributes().style.color)
+    const styleObj = fromStringStyleToObject(serviceButtonTitleNode.at(0).attributes().style)
+    expect(styleObj.color).toEqual(data.titleColor)
+  })
+})
