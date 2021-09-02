d21ead84021d2db5ea7ae5f6c7806ae3dcd80655
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2021-09-02 10:02:44 +0900

    ・映画スケジュール（スクリーン）コンポーネントを実装する。

--- src/components/movie/MovieScheduleScreen.vue
+++ src/components/movie/MovieScheduleScreen.vue
@@ -1,5 +1,42 @@
 <template>
-  <div></div>
+  <div>
+    <div class="MovieScheduleScreenDate">
+      {{ data.month + "月" + data.day + "日(" + data.d + ")"}}
+    </div>
+    <div class="MovieScheduleScreenTitle">{{ data.title }}</div>
+    <div>
+      <div
+        v-for="(movieScheduleScreen, movieScheduleScreenIdx) in data.screen"
+        :key="'MovieScheduleScreen' + movieScheduleScreenIdx">
+        <div class="MovieScheduleScreenName">SCREEN{{ movieScheduleScreen.screen }}</div>
+        <div>
+          <div
+            v-for="(movieScheduleScreenTerm, movieScheduleScreenTermIdx) in movieScheduleScreen.term"
+            :key="'MovieScheduleScreenTerm' + movieScheduleScreenTermIdx"
+            class="MovieScheduleScreenTerm">
+            <div>
+              <span>
+                {{
+                  String(movieScheduleScreenTerm.startHour).padStart(2, '0') +
+                  ":" +
+                  String(movieScheduleScreenTerm.startMinute).padStart(2, '0')
+                }}
+              </span>
+              <span> ~ </span>
+              <span>
+                {{
+                  String(movieScheduleScreenTerm.endHour).padStart(2, '0') +
+                  ":" +
+                  String(movieScheduleScreenTerm.endMinute).padStart(2, '0')
+                }}
+              </span>
+            </div>
+            <div>〇販売中</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
---
+++ src/components/movie/__tests__/MovieScheduleScreen.spec.js
@@ -0,0 +1,54 @@
+import { shallowMount } from '@vue/test-utils'
+import { movieScheduleScreenData } from '../MovieScheduleScreenData'
+import MovieScheduleScreen from '@/components/movie/MovieScheduleScreen'
+
+describe('MovieScheduleScreenコンポーネント', () => {
+  it('プロップスのテスト', () => {
+    const data = movieScheduleScreenData
+    const wrapper = shallowMount(
+      MovieScheduleScreen,
+      {
+        propsData: {
+          data
+        }
+      }
+    )
+
+    const movieScheduleScreenDateNode = wrapper.findAll('div.MovieScheduleScreenDate')
+    expect(movieScheduleScreenDateNode).toHaveLength(1)
+    const movieScheduleScreenDate =
+      data.month +
+      '月' +
+      data.day +
+      '日(' +
+      data.d +
+      ')'
+    expect(movieScheduleScreenDateNode.at(0).text()).toEqual(movieScheduleScreenDate)
+
+    const movieScheduleScreenTitleNode = wrapper.findAll('div.MovieScheduleScreenTitle')
+    expect(movieScheduleScreenTitleNode).toHaveLength(1)
+    expect(movieScheduleScreenTitleNode.at(0).text()).toEqual(data.title)
+
+    var screenList = []
+    var termList = []
+    data.screen.forEach(function (s) {
+      screenList.push(s)
+      s.term.forEach(function (t) {
+        termList.push(t)
+      })
+    })
+    const movieScheduleScreenNameNodes = wrapper.findAll('div.MovieScheduleScreenName')
+    expect(movieScheduleScreenNameNodes).toHaveLength(screenList.length)
+    screenList.forEach(function (s, sIdx) {
+      const movieScheduleScreenNameNode = movieScheduleScreenNameNodes.at(sIdx)
+      expect(movieScheduleScreenNameNode.text()).toEqual('SCREEN' + s.screen)
+    })
+    const movieScheduleScreenTermNodes = wrapper.findAll('div.MovieScheduleScreenTerm')
+    expect(movieScheduleScreenTermNodes).toHaveLength(termList.length)
+    termList.forEach(function (t, tIdx) {
+      const movieScheduleScreenTermNode = movieScheduleScreenTermNodes.at(tIdx)
+      expect(movieScheduleScreenTermNode.text()).toContain(String(t.startHour).padStart(2, '0') + ':' + String(t.startMinute).padStart(2, '0'))
+      expect(movieScheduleScreenTermNode.text()).toContain(String(t.endHour).padStart(2, '0') + ':' + String(t.endMinute).padStart(2, '0'))
+    })
+  })
+})
