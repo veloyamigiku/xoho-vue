18f1285b10969368cf818ac0d6948d66e5f6a53a
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2021-11-08 15:07:22 +0900

    ・劇場（IMAX）の本体のスタイルを修正する。本体の背景にスタイルを適用する。

--- src/components/theater/imax/ImaxTop.vue
+++ src/components/theater/imax/ImaxTop.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>ImaxTop</div>
+  <div class="ImaxTop">
+  </div>
 </template>
 
 <script>
@@ -9,4 +10,7 @@ export default {
 </script>
 
 <style scoped>
+div.ImaxTop {
+  background: url("https://lh3.googleusercontent.com/pw/AM-JKLU7SLZjcYu1yOU8jk0g8Kr87GyjJA1TB2bf-XnXu3Ek84SXPHt0ze3dsmKsAqY9SkE2VHLlxdulh7q0B01TTsbgBwgoqAGRpff1WMa1CH6MaBtGAAp_Y4lEnix9cbLQZatKA40fofQnwviME5s_0aUL=w1368-h10-no?authuser=0") repeat-y center #012238;
+}
 </style>
