bdf453fd1bf7069172548daa56d965b5f11d5e5e
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2021-11-29 08:19:59 +0900

    ・劇場のテストデータを修正する。イントロ画像URLの項目を追加する。

--- src/components/theater/TheaterData.js
+++ src/components/theater/TheaterData.js
@@ -69,7 +69,10 @@ export const theaterData = [
                   'allergies'
                 ]
               }
-            ]
+            ],
+            introImgUrl: {
+              1: 'https://lh3.googleusercontent.com/pw/AM-JKLUc9Khxn4g7y3pnVg_59FMxqtxtG-mrBYJepZpdqVpJqCy-_FiAoefz5SCwEQV7zB6aycnVyc8-K9va4gyQw1MZsAZ0P80ClZyzigS0qelDFDdmMyQO96hsXUIj_SboNHe6b-Mu_4VMCJfyJ9ygEk1I=w932-h79-no?authuser=0'
+            }
           }
         ]
       }
@@ -133,7 +136,10 @@ export const theaterData = [
                   'allergies'
                 ]
               }
-            ]
+            ],
+            introImgUrl: {
+              1: 'https://lh3.googleusercontent.com/pw/AM-JKLUq8YGhK5tUukUeSX9uXlbjYszg2_wS8Lpns872LO8gPXy6Aw-lbTpNKp_Te-uYfGl5Ty2LM1HDT4xpBxGH4ADSu5G_GZs1PIFnOeQ1J8Ofn3TglWNXOfXNmPofFEIQ867sDL2-JSyg4W4zbPEHRubB=w932-h79-no?authuser=0'
+            }
           },
           {
             name: 'シャンテ',
@@ -186,7 +192,10 @@ export const theaterData = [
                   'air-freshener'
                 ]
               }
-            ]
+            ],
+            introImgUrl: {
+              6: 'https://lh3.googleusercontent.com/pw/AM-JKLVAlcUFsK78VLi_sT3HLPPe5fM6o5LhDfBJ-UztsmehlK2orQyJ9Mq1hio6Riq-tIo8KTpea5DooGNLsyu0EQVLmD7cieqxPsxhW-YyMJwGyd7uHIe4ICmx1xz6oG6sP5tejhiliZJTHtLYstSnUJc_=w932-h79-no?authuser=0'
+            }
           },
           {
             name: '池袋',
@@ -356,7 +365,10 @@ export const theaterData = [
                   'allergies'
                 ]
               }
-            ]
+            ],
+            introImgUrl: {
+              1: 'https://lh3.googleusercontent.com/pw/AM-JKLXUvMoSlG14N6np7Yb56oOAUOlSm1sOskFjViwCSveVsnBfx4s55IhaCx11fY_TXaRFDQJiatAtXB_7W_EcIwUgGdS_v-CIVI2l0uivJpcIE7O54w0YlwW_GCxbkY4CWsFb6RSEPJe9Cazk7xEkXG3V=w932-h79-no?authuser=0'
+            }
           },
           {
             name: '錦糸町（楽天地・オリナス）',
@@ -471,7 +483,10 @@ export const theaterData = [
                   'american-sign-language-interpreting'
                 ]
               }
-            ]
+            ],
+            introImgUrl: {
+              6: 'https://lh3.googleusercontent.com/pw/AM-JKLUHiA4PInw8DkmgAlCU4rdCEociAhIObNClrRlwJg2SDHKf6KfbF5TdKm22uQC5L48GyBBtViC5F-jYOEgChhuqt3WBnuCgWwZw6jZNIs033wRmMCoXGsqTRTqXRiiXBHMrUjPjbzgXC0j2YvHI3smg=w932-h79-no?authuser=0'
+            }
           },
           {
             name: '市原',
@@ -561,7 +576,10 @@ export const theaterData = [
                   'air-freshener'
                 ]
               }
-            ]
+            ],
+            introImgUrl: {
+              1: 'https://lh3.googleusercontent.com/pw/AM-JKLXruI8GW-AxIoNClxo98HG5BAQqHa7unyK4_6-wXkEv_oLZ8JbjjNfD_k1zI69SSYGJPwjuzklOZceXWcNv4pVER7fMRHNI_WIsgT1jI2U53BCtOHGiqJX12uXgXEWlu2GTNlLOloMa1S_C0cooJOOd=w932-h79-no?authuser=0'
+            }
           },
           {
             name: '上大岡',
@@ -862,7 +880,10 @@ export const theaterData = [
                   'air-freshener'
                 ]
               }
-            ]
+            ],
+            introImgUrl: {
+              1: 'https://lh3.googleusercontent.com/pw/AM-JKLXN49v5Bq2Mt8V4aQCYNWWKpNYtInjEhQqHqOdoea-cLFkKq9uo5Rp37-2qt3I9OsZJmCvUvfWRpRpML4bRhes3NGr8rLG505Pl0gaLXngWNIJeJKtFGfV51e7-X0zaynFkjoYBpz7TB1Ze4RMv5o-9=w932-h79-no?authuser=0'
+            }
           },
           {
             name: '泉北',
@@ -945,7 +966,10 @@ export const theaterData = [
                   'air-freshener'
                 ]
               }
-            ]
+            ],
+            introImgUrl: {
+              1: 'https://lh3.googleusercontent.com/pw/AM-JKLWAwatxM5fxDw_gf9H8dlytwedLxpkNCNhWXY3gmi66NdEhKckC-1AyQnZsNnsLkQak7Sn4YUv9OzemSESRNH6nHGo_TSdh6LGBd6MUnh98hZEUAXpnFDan1AZinEZn7JHYmIWfUm3rUBwXJkpZMmeE=w932-h79-no?authuser=0'
+            }
           }
         ]
       },
@@ -967,8 +991,21 @@ export const theaterData = [
                   'fas',
                   'air-freshener'
                 ]
+              },
+              {
+                name: 'IMAXレーザー',
+                title: 'IMAXレーザー劇場一覧',
+                sub: '最高級のプレミアムシアター',
+                option: 'IMAX®レーザーとは？',
+                icon: [
+                  'fas',
+                  'american-sign-language-interpreting'
+                ]
               }
-            ]
+            ],
+            introImgUrl: {
+              6: 'https://lh3.googleusercontent.com/pw/AM-JKLXo-i1U9crvDSy_9wV0-D15zDsUjX_lAvc_fTecbeO8cxCjPPZMQqSlzMT2aYV6ez0gqjmsYtB3gwlOHsWjYb4tEhdZBmeZYhvJWDu8N7L5BVd32aHkJCP5zCMZKWUUGs64UeETD9M5UJHeziMsKUl7=w932-h79-no?authuser=0'
+            }
           },
           {
             name: '伊丹',
@@ -994,7 +1031,7 @@ export const theaterData = [
   },
   {
     header: {
-      title: '中国地方',
+      title: '中国地区',
       sub: 'CHUGOKU AREA',
       option: ''
     },
