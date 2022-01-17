01212874fe21abf47b24d22586d31b3c20a8db9f
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2022-01-17 16:50:17 +0900

    ・IMAX情報（テキスト）コンポーネントに、スタイルを適用する。

--- src/components/theater/imax/ImaxInfoText.vue
+++ src/components/theater/imax/ImaxInfoText.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="ImaxInfoText">
     <div class="ImaxInfoTextHeader">{{ data.header }}</div>
     <div class="ImaxInfoTextBody">{{ data.body }}</div>
   </div>
@@ -15,4 +15,28 @@ export default {
 </script>
 
 <style scoped>
+div.ImaxInfoText {
+  margin: 40px auto 0 auto;
+  max-width: 940px;
+}
+
+div.ImaxInfoTextHeader {
+  padding: 10px 0;
+  color: #fff;
+  background-color: #0d5b88;
+  font-family: 'M PLUS 1p';
+  font-size: 38px;
+  font-weight: 400;
+  text-align: center;
+}
+
+div.ImaxInfoTextBody {
+  padding: 55px 30px;
+  color: #fff;
+  background-color: #020925;
+  font-family: 'M PLUS 1p';
+  font-size: 28px;
+  font-weight: 300;
+  letter-spacing: -0.025em;
+}
 </style>
