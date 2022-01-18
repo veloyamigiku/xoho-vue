757d910c15d50f8fc3b8a97ddb1648ce038a654f
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2022-01-19 08:31:04 +0900

    ・IMAX情報（画像）コンポーネントに、スタイルを適用する。

--- src/components/theater/imax/ImaxInfoImage.vue
+++ src/components/theater/imax/ImaxInfoImage.vue
@@ -1,28 +1,30 @@
 <template>
-  <div>
-    <div>
-      <img
-        class="ImaxInfoImageTitle"
-        :src="data.titleImgUrl"
-        alt="ImaxInfoImageTitle" />
-    </div>
-    <div>
-      <img
-        class="ImaxInfoImageSubTitle"
-        :src="data.subTitleImgUrl"
-        alt="ImaxInfoImageSubTitle" />
-    </div>
-    <div>
-      <img
-        class="ImaxInfoImageImg"
-        :src="data.imgUrl"
-        alt="ImaxInfoImageImg" />
-    </div>
-    <div>
-      <img
-        class="ImaxInfoImageText"
-        :src="data.textImgUrl"
-        alt="ImaxInfoImageText" />
+  <div class="ImaxInfoImageWrap">
+    <div class="ImaxInfoImage">
+      <div class="ImaxInfoImageTitleWrap">
+        <img
+          class="ImaxInfoImageTitle"
+          :src="data.titleImgUrl"
+          alt="ImaxInfoImageTitle" />
+      </div>
+      <div class="ImaxInfoImageSubTitleWrap">
+        <img
+          class="ImaxInfoImageSubTitle"
+          :src="data.subTitleImgUrl"
+          alt="ImaxInfoImageSubTitle" />
+      </div>
+      <div class="ImaxInfoImageImgWrap">
+        <img
+          class="ImaxInfoImageImg"
+          :src="data.imgUrl"
+          alt="ImaxInfoImageImg" />
+      </div>
+      <div class="ImaxInfoImageTextWrap">
+        <img
+          class="ImaxInfoImageText"
+          :src="data.textImgUrl"
+          alt="ImaxInfoImageText" />
+      </div>
     </div>
   </div>
 </template>
@@ -37,4 +39,29 @@ export default {
 </script>
 
 <style scoped>
+div.ImaxInfoImageWrap {
+  border-bottom: 1px solid #404f66;
+}
+
+div.ImaxInfoImage {
+  max-width: 940px;
+  margin: 0 auto;
+  text-align: center;
+}
+
+div.ImaxInfoImageTitleWrap {
+  padding: 38px 0;
+}
+
+div.ImaxInfoImageSubTitleWrap {
+  padding-bottom: 64px;
+}
+
+div.ImaxInfoImageImgWrap {
+  padding-bottom: 57px;
+}
+
+div.ImaxInfoImageTextWrap {
+  padding-bottom: 78px;
+}
 </style>
